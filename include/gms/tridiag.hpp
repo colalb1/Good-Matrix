e0b966b0a35d73e23ecbbe32659ac44f7f989fe9
Colin <34983726+colalb1@users.noreply.github.com>
2025-08-03 14:00:26 -0500

    Thomas algorithm.

--- include/gms/tridiag.hpp
+++ include/gms/tridiag.hpp
@@ -35,15 +35,18 @@ void thomas_solve(const T *a, const T *b, const T *c, const T *d, T *x,
 
   for (std::size_t i = 1; i < n; ++i) {
     const T denom = b[i] - a[i] * c_prime[i - 1];
+
     if (std::abs(denom) <= std::numeric_limits<T>::epsilon()) {
       throw std::runtime_error("thomas_solve: zero pivot encountered");
     }
+
     c_prime[i] = (i < n - 1) ? c[i] / denom : T{0};
     d_prime[i] = (d[i] - a[i] * d_prime[i - 1]) / denom;
   }
 
   // Back substitution
   x[n - 1] = d_prime[n - 1];
+  
   for (std::ptrdiff_t i = static_cast<std::ptrdiff_t>(n) - 2; i >= 0; --i) {
     x[i] = d_prime[i] - c_prime[i] * x[i + 1];
   }
